7334df9894549a88d2a3d95b3217e321571e58e2
Veeed <116961581+Veeed@users.noreply.github.com>
2022-11-03 22:48:47 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+ camera = document.getElementById("camera");
+
+ Webcam.set({
+    width: 360,
+    height: 250,
+    image_format:'png',
+    png_quality: 90
+  });
+ 
+ function start (){
+    Webcam.attach(camera);
+ }
+
+
+ 
